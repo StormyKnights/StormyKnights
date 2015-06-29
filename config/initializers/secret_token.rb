4666153aeb157135d11ca9c95a0e833d7e7aee21
Yury Voloshin <yvoloshin01@gmail.com>
2015-06-29 20:26:06 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StormyKnights::Application.config.secret_key_base = 'fb8187598c38f6e25115be1a3f7ad63fe6ea431d48f0237db3bd0cb753f0d09482f487e6f096bcc93a0c76a707fd2408bd98d18745d650b9dea8977a509d3ae3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StormyKnights::Application.config.session_store :cookie_store, key: '_stormy_knights_session'
