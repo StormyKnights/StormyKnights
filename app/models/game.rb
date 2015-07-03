b9aa3a77e3d74bc21d5995bf2d93e78f3078014c
Colin Rubbert <colinrubbert@gmail.com>
2015-07-03 01:18:33 +0000

    Install Factory girl in preperation for association testing

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,7 @@ class Game < ActiveRecord::Base
   has_many :users
   has_many :pieces
 
+  belongs_to :white_user, :class_name => "User", :foreign_key => "white_user_id"
+  belongs_to :black_user, :class_name => "User", :foreign_key => "black_user_id"
+
 end
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,7 +1,5 @@
 class Piece < ActiveRecord::Base
 
   belongs_to :game
-  belongs_to :white_user_id, :class_name => "User", :foreign_key => "white_user_id"
-  belongs_to :black_user_id, :class_name => "User", :foreign_key => "black_user_id"
 
 end
