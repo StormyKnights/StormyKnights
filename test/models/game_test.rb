8507ec3595dd125ff798aa4c12f6ba37ec22210d
Colin Rubbert <colinrubbert@gmail.com>
2015-07-04 22:43:13 +0000

    Add test for asserting that the board was populated

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,5 +1,5 @@
 require 'test_helper'
-require 'pry-rails'
+
 
 class GameTest < ActiveSupport::TestCase
    test "the truth" do
@@ -8,10 +8,10 @@ class GameTest < ActiveSupport::TestCase
 
    test "foreign_key associations" do
 
-      User.create(:email => "a@example.com", :password => "123456", :password_confirmation => "123456").save(:validate => false)
-      User.create(:email => "b@example.com", :password => "123456", :password_confirmation => "123456").save(:validate => false)
+      User.create(email: "a@example.com", password: "123456", password_confirmation: "123456").save(validate: false)
+      User.create(email: "b@example.com", password: "123456", password_confirmation: "123456").save(validate: false)
 
-      g = Game.create(:black_user_id => "1", :white_user_id => "2")
+      g = Game.create(black_user_id: "1", white_user_id: "2")
       g = Game.last # loading last game into a variable
 
       u1 = User.first # loading user_id 1 as black_user
@@ -25,6 +25,17 @@ class GameTest < ActiveSupport::TestCase
 
       assert_equal u1, g.black_user
       assert_equal u2, g.white_user
+
+   end
+
+   test "board population" do
+
+     g = Game.create(white_user_id: "1", black_user_id: "2")
+
+     assert_equal 32, g.pieces.count
+     assert_equal "King", g.pieces.last.name
+     assert_equal 4, g.pieces.last.x_coordinates
+     assert_equal 7, g.pieces.last.y_coordinates
 
    end
 
