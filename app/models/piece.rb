10bbf2c6d2cca4447581ce3cf7fb8b1cdb160974
Vu Pham <vupham0326@mail.com>
2015-07-08 15:52:01 +0000

    created the submodels for STI and created a new migration to add type column to the pieces table

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,4 +2,14 @@ class Piece < ActiveRecord::Base
 
   belongs_to :game
 
+  # adding scope and delegate to make it easier to navigate between the models
+  # scope :pawns, -> { where(type: 'Pawn') } 
+  # scope :bishops, -> { where(type: 'Bishop') }
+  # scope :knights, -> { where(type: 'Knight') }
+  # scope :rooks, -> { where(type: 'Rook') }
+  # scope :queens, -> { where(type: 'Queen') }
+  # scope :kings, -> { where(type: 'King') }
+
+  # delegate :pawns, :bishops, :knights, :rooks, :queens, :kings, to: :pieces
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150702194958) do
+ActiveRecord::Schema.define(version: 20150708141251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,12 +24,12 @@ ActiveRecord::Schema.define(version: 20150702194958) do
   end
 
   create_table "pieces", force: true do |t|
-    t.string   "name"
     t.integer  "x_coordinates"
     t.integer  "y_coordinates"
     t.integer  "game_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "type"
   end
 
   create_table "users", force: true do |t|
