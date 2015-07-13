caacda991bf2902bf311bf6eaafc1efba2725057
Vu Pham <vupham0326@mail.com>
2015-07-13 20:40:57 +0000

    add the start game button and link pieces#show

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,8 +1,26 @@
 class PiecesController < ApplicationController
+  
   def create
+  	@piece = Piece.new # set new piece?
   end
 
   def update
+  	@piece = Piece.find(params[:id])
+  	@piece.update_attributes(piece_params) #correct?
+  	redirect_to game_path(@piece.game)  #
+  	
+  end
+
+  def show
+	  @piece = Piece.find(params[:id])
+    @pieces = @piece.game.pieces 
+ 
+  end
+
+  private
+
+  def piece_params
+  	params.require(:piece).require(:x_coordinates, :y_coordinates)
   end
 
 end
