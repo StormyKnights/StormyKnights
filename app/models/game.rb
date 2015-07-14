9fbb1d225c176a95788386ed4415aab6905e5fb4
Yury Voloshin <yvoloshin01@gmail.com>
2015-07-14 13:07:09 +0000

    Added is_obstructed

--- app/models/game.rb
+++ app/models/game.rb
@@ -44,4 +44,80 @@ class Game < ActiveRecord::Base
 
   end
 
+  #determines whether the square with coordinates (x, y) is occupied
+  def is_occupied?(x, y) 
+    pieces.each do |piece|
+      if piece.x_coordinates == x && piece.y_coordinates == y
+        return true
+      end
+    end
+    return false
+  end
+
+  #determines whether the path between piece1 and destination is obstructed by another piece
+  def is_obstructed?(piece1_loc, destination)
+    #converts the location arrays into easier-to-read x and y terms
+    x1 = piece1_loc[0]
+    y1 = piece1_loc[1]
+    x2 = destination[0]
+    y2 = destination[1]
+    
+    # Determines whether the line between piece1 and the destination is horizontal or
+    # vertical. If neither, then it calculates the slope of line between piece1 and destination.
+    if y1 == y2
+      horizontal = true
+    elsif x1 == x2
+      vertical = true
+    else
+      slope = (y2-y1).to_f/(x2-x1).to_f
+    end
+
+    # iterates through every square between piece1 and destination
+    # and checks whether it is occupied
+    if horizontal && x1 < x2
+      (x1+1..x2-1).each do |x|
+        return true if is_occupied?(x, y1)
+      end
+    end
+    if horizontal && x1 > x2
+      (x2+1..x1-1).each do |x|
+        return true if is_occupied?(x, y1)
+      end
+    end
+    if vertical && y1 < y2
+      i = y1+1
+      while i < y2 do
+        return true if is_occupied?(x1, i)
+        i += 1
+      end
+    end
+    if vertical && y1 > y2
+      (y2+1..y1-1).each do |y|
+        return true if is_occupied?(x1, y)
+      end
+    end
+    if horizontal || vertical
+      return false
+    end
+    if slope.abs == 1.0 && x1 < x2
+      (x1+1..x2-1).each do |x|
+        delta_y = x-x1
+        y = y2 > y1 ? y1 + delta_y : y1 - delta_y
+        return true if is_occupied?(x, y)
+      end
+    end
+    if slope.abs == 1.0 && x1 > x2
+      (x2+1..x1-1).each do |x|
+        delta_y = x1-x
+        y = y2 > y1 ? y1 + delta_y : y1 - delta_y
+        return true if is_occupied?(x, y)
+      end
+    end
+    if (slope.abs != 1.0)
+      fail 'path is not a straight line'
+    else return false
+    end
+  end
+
+
 end
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -38,4 +38,22 @@ class GameTest < ActiveSupport::TestCase
 
    end
 
+   test "is_occupied" do
+
+    g = Game.create(white_user_id: 1, black_user_id: 2)
+    
+    assert_equal true, g.is_occupied?(4, 7)
+    assert_equal true, g.is_obstructed?([0,0], [3,0])
+    assert_equal true, g.is_obstructed?([1,0], [1,3])
+    assert_equal true, g.is_obstructed?([2,0], [4,2])
+    assert_equal true, g.is_obstructed?([4,2], [2,0])
+    assert_equal false, g.is_obstructed?([0,2], [2,4])
+    assert_equal false, g.is_obstructed?([1,3], [5,3])
+    assert_equal false, g.is_obstructed?([4,2], [4,5])
+    assert_raise RuntimeError do
+      g.is_obstructed?([3,0], [4,2])
+    end
+
+   end
+
 end
