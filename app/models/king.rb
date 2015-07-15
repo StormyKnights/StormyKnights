3e597c0095f9e9a9a67e35f5e7494e1215b3f668
Colin Rubbert <colinrubbert@gmail.com>
2015-07-15 02:02:42 +0000

    Add move validation for the King

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,3 +1,11 @@
 class King < Piece
-	
+
+	def valid_move?(x_destination, y_destination)
+		x_distance = (x_coordinates - x_destination).abs
+		y_distance = (y_coordinates - y_destination).abs
+
+		valid_destinations = ([0,1],[1,0],[1,1])
+		valid_destinations.include?(x_distance) && valid_destinations.include?(y_distance)
+	end
+
 end
