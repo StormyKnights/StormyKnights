4931561b67363ec3628c742a2413ce4e22acacdf
Colin Rubbert <colinrubbert@gmail.com>
2015-07-16 00:41:03 +0000

    Update valid king move and create test, is_obstructed not implemented yet

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,11 +1,10 @@
 class King < Piece
 
 	def valid_move?(x_destination, y_destination)
-		x_distance = (x_coordinates - x_destination).abs
-		y_distance = (y_coordinates - y_destination).abs
+		x_distance = (x_coordinates - x_destination).abs <=1
+		y_distance = (y_coordinates - y_destination).abs <=1
 
-		valid_destinations = ([0,1],[1,0],[1,1])
-		valid_destinations.include?(x_distance) && valid_destinations.include?(y_distance)
+		x_distance && y_distance
 	end
 
 end
