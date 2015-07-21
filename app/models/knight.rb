2ba6c36b5b22925c9ccb5fb5a111d0c4fa2c5fb0
Colin Rubbert <colinrubbert@gmail.com>
2015-07-21 22:41:52 +0000

    Add valid move for Knight w/o obstruction

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,3 +1,8 @@
 class Knight < Piece
-	
+	def valid_move?(x_destination, y_destination)
+		x_distance = (x_coordinates - x_destination).abs
+		y_distance = (y_coordinates - y_destination).abs
+
+		(x_distance == 2 && y_distance == 1) || (x_distance == 1 && y_distance == 2)
+	end
 end
--- test/models/piece_test.rb
+++ test/models/piece_test.rb
@@ -29,4 +29,37 @@ class PieceTest < ActiveSupport::TestCase
 
   end
 
+  test "valid knight move" do
+
+    g = Game.create(white_user_id: 1, black_user_id: 2)
+    knight = g.pieces.find_by(type: 'Knight', color: 'white', x_coordinates: 1, y_coordinates: 0)
+
+    # Valid Moves From Starting position (1, 0)
+    assert_equal true, knight.valid_move?(3,1)  # Move 1 up and 2 right from starting position
+    assert_equal true, knight.valid_move?(2,2)  # Move 2 up and 1 right from starting position
+    assert_equal true, knight.valid_move?(0,2)  # Move 2 up and 1 left from starting position
+
+    knight = g.pieces.find_by(type: 'Knight', color: 'white', x_coordinates: 6, y_coordinates: 0)
+
+    # Valid Moves From Starting position (6, 0)
+    assert_equal true, knight.valid_move?(7,2)  # Move 2 up and 1 right from starting position
+    assert_equal true, knight.valid_move?(5,2)  # Move 2 up and 1 left from starting position
+    assert_equal true, knight.valid_move?(4,1)  # Move 2 up and 1 left from starting position
+
+    knight = g.pieces.find_by(type: 'Knight', color: 'black', x_coordinates: 1, y_coordinates: 7)
+
+    # Valid Moves From Starting position (1, 7)
+    assert_equal true, knight.valid_move?(0,5)  # Move 2 down and 1 left from starting position
+    assert_equal true, knight.valid_move?(2,5)  # Move 2 down and 1 right from starting position
+    assert_equal true, knight.valid_move?(3,6)  # Move 1 down and 2 right from starting position
+
+    knight = g.pieces.find_by(type: 'Knight', color: 'black', x_coordinates: 6, y_coordinates: 7)
+
+    # Valid Moves From Starting position (6, 7)
+    assert_equal true, knight.valid_move?(7,5)  # Move 2 down and 1 right from starting position
+    assert_equal true, knight.valid_move?(5,5)  # Move 2 down and 1 left from starting position
+    assert_equal true, knight.valid_move?(4,6)  # Move 1 down and 2 left from starting position
+
+  end
+
 end
