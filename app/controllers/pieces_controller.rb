772b7e72c32d140a24584b25d33cad994b2757e6
yoshinaria <yoshinariakiko@gmail.com>
2015-07-30 13:52:11 -0500

    Fixed ajax window refresh

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -10,19 +10,21 @@ class PiecesController < ApplicationController
     if @piece.valid_move?(x_coordinates, y_coordinates)
       @piece.move_to!(x_coordinates, y_coordinates)
       if @piece.update_attributes(x_coordinates: params[:x_coordinates], y_coordinates: params[:y_coordinates]) #move the pieces by passing in x,y coordinates
-    	   flash[:notice] = "Move made"  # no errors move successful
+    	   # flash[:notice] = "Move made"  # no errors move successful
+         @valid = true
       end
     else
-      flash[:alert] = 'Move is invalid'
+      # flash[:alert] = 'Move is invalid'
+      @valid = false
     end
     
-
     respond_to do |format|
       format.html do 
         redirect_to game_path(@game)  #redirect to game show page
       end
       format.json do
-        render json: params
+        json_result = {:valid => @valid, :status => @status}
+        render json: json_result
       end
     end
 
